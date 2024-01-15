4a3ee71808d6c026062f41d128b87f9088f53a53
keshavVarynya <keshav.yadav@varynya.care>
2024-01-15 11:48:20 +0530

    change in no_chemical_fachials blog

--- src/app/blog/No_Chemical_Fachials/page.tsx
+++ src/app/blog/No_Chemical_Fachials/page.tsx
@@ -11,56 +11,88 @@ const NoChemical: NextPage = () => {
                 <Image className="hairOlingFullBlog" src={crousalPick} alt="..." />
 
             </div>
-            <div className="text-center pt-5"><span className="underline underline-offset-8 leading-relaxed tracking-wide font-bold text-xl text-stone-900 blogContentHeading">Hair Oiling: A Comeback</span></div>
-            <div className="p-10 pb-0 "><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">Hair oiling is one of the oldest tricks in the book for long, healthy, and luscious locks. Even though trends have changed today, oiling used to be a go-to for our grandmothers and
-                mothers. With innumerable chemical treatments, including keratin, botox, nanoplasty, and
-                whatnot, most people seem to have forgotten the good ol’ oil treatment. </span></div>
-            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">Be it excessive heat damage, split ends, or dull tresses, oiling hair never disappoints. In
-                addition, environmental factors such as increased pollution and climatic changes highly
-                affect hair quality. However, one can easily restore the natural shine and luster of the hair
-                by regularly nourishing it with the proper nutrients, and patience, of course! Want
-                jaw-droppingly voluminous hair like Rapunzel? Oil it! </span></div>
-            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">One of the fondest childhood memories for most Asians would be their mother oiling their
-                hair. Those days and memories just hit differently. Those days might not come back, but
-                your pre-chemical hair can. Yes, with the right oil and techniques, you might be able to
-                flaunt some real-gorgeous tresses.  </span></div>
-            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">Now, if you are reading carefully, you might wonder what we mean by the “right oil”? By
-                right, we simply mean pure and unadulterated. Whenever we consume resources in their
-                purest form, great results are promised. You can use coconut, almond, sesame, olive, or
-                argan oil, just ensure it's untainted.</span></div>
-            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">Aggressive chemicals and related treatments leave hair brittle and dry. Aside from this,
-                your hair will eventually lose its ability to lock natural moisture. You can bid adieu to the
-                shine and strength your hair once possessed. Didn’t know better before? It’s alright. Almost
-                all chemical-centric treatments, including bleaching, decoloration, smoothening, and
-                several others, excessively deplete hair quality. The result of relentless chemical dosage can
-                be irreparable damage - one of the most horrid nightmares for a woman. It’s like having a
-                bad hair day every day. Don’t worry, you can still reverse the situation.</span></div>
-            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">With a balanced and protein-rich diet, you can do wonders for your hair. However, in this
-                case, what you apply to your hair is more important. Regular and deep nourishment of the
-                scalp and tresses can completely change your hair game. Virgin oils, rich in vitamins A and
-                E and antioxidants, can aid by offering much-needed solace to your once-lovely mane. Your
-                locks are one of the most eye-catching features. Make sure you hold people’s attention for
-                the right reason, that is, incredibly envious hair.</span></div>
-            <div className="p-10 pb-0"><span className="font-normal text-2xl text-neutral-500 tracking-wide blogContent">Here is how virgin oils can up your hair game</span></div>
+            <div className="text-center pt-5"><span className="underline underline-offset-8 leading-relaxed tracking-wide font-bold text-xl text-stone-900 blogContentHeading">No Chemical Facials</span></div>
+            <div className="p-10 pb-0 "><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">Thinking to yourself if you know exactly what this article is about? Is the topic really giving
+it all away? Well, you are right and wrong. This 3-min read focuses on chemical-free facials,
+sure, but it also sheds light on numerous other things you might be doing wrong, skin-wise.
+Let’s dive deep into it to give you a clearer picture.</span></div>
+            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">In the present scenario, skin-care has overtaken makeup and cosmetic beauty treatments.
+More and more people seek to invest in branded products to up their skin game, but do
+they really know what’s inside those toners, cleansers, or moisturizers? Sure, it feels good
+in an instant, but what about the harm? Now you must be thinking - what harm? How can a
+cleanser or cream harm me? Well, welcome to the age of ENLIGHTENMENT!!! </span></div>
+            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">Gave enough thought to how a product can change your skin tone in a week? How do these
+companies CLAIM their products are capable of these? Well, it is time we look up their
+ingredients. Just like you read up before buying groceries, why don’t we emphasize the
+need to do the same for our skin and hair?</span></div>
+            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">All individuals have different ways of unwinding. While some prefer to spend their day off
+in the great outdoors, others seek self-pampering. Thankfully, there are a lot of outlets
+available for that. A trip to their favorite salon would most definitely top this list. And
+nothing says relaxation and leaving your stress behind like a good facial! Your skin is prone
+to pollution, heat, dust and at least a minimal makeup routine throughout the week. A
+calming and long facial can definitely do the trick. Here’s a question - do you go for
+conventional facials or chemical-free ones?</span></div>
+            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">We hear chemical-free facials are the one wherein naturally derived ingredients are used in
+the products. Sounds better for your skin and health, no? We call this the age of
+enlightenment because a larger section is now concerned about what is going on inside
+their bodies. It may come as a shock to some, but all major beauty brands use ingredients in
+their products that are either unsafe for the human body or the environment. Regardless of
+their claims, there have been innumerable instances where toxic chemicals have been found
+in the products - sulfates, paraben, phthalates, and lead, to name a few. But there is nothing
+that money cannot settle, big or small lawsuits included, right?</span></div>
+<div className="p-10 pb-0"><span className="font-normal text-2xl text-cyan-600 tracking-wide blogContent">Why are chemical products yielding results on my skin then?</span></div>
+
+            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">Have you ever wondered why your skin glows instantly after a facial? Why is there a world
+of difference in your before and after images? There is definitely no magic in the hands of
+your esthetician, but it is the ingredients of the products used by them. Just like thenaturally-derived ingredients of chemical-free products take time to develop, so do their
+results. While on the other hand, chemical-rich products are quick to yield results and
+hence are still widely chosen.</span></div>
+<div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">We think it is safe to say that we are living in an age of instant gratification and need
+everything instantly, including the glow on our faces. But let’s not be blindsided to the
+harmful side-effects of applying chemical additives. Believe us if you will, they aren’t doing
+you any good. If aging gracefully is something you seek, you might want to rethink your
+choices before it’s too late!</span></div>
+<div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">Everything you do for your health, skin, hair and body should be an investment rather than
+an expense. It is the nature of investments to offer benefits, whether long or short term. To
+understand better, let’s look at an instance. If you regularly order junk food, you are adding
+to your expenses along with the added health risk. On the contrary, when you eat balanced
+meals, rich in fiber, protein and all the required goodness, you are investing in your health
+and increasing longevity. Same way, your skin absorbs anything and everything you feed it.</span></div>
+<div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">Don’t know how to make the switch? Just start by simply ensuring what you eat and what
+you apply share a few commonalities. We’re sure there are hardly any chemicals in your
+food with weird names and even weirder pronunciations!</span></div>
+<div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">Among the countless benefits of using naturally-driven products and ingredients, radiant
+skin sort of tops the list. And who doesn’t love blindingly-glowing skin? Just like good food
+and water do wonders for your skin, the same way organic products work. The more raw
+and familiar-sounding products you apply, the better!</span></div>
+            <div className="p-10 pb-0"><span className="font-normal text-2xl text-neutral-500 tracking-wide blogContent">Following below are the reasons why organic products are a godsend for you</span></div>
             <div className="p-5 pb-0 ps-20"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">
-                • Deep nourishment and hydration<br></br>
-                • Offer essential vitamins and antioxidants <br></br>
-                • Leave you with strong and manageable locks<br></br>
-                • Reduced scalp irritation and dandruff issues<br></br>
-                • Act as a chemical-free conditioner<br></br>
-                • Shiny and lustrous hair<br></br>
-                • Reverse chemical and heat damage to some extent</span></div>
-            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">As mentioned above, you can apply any oil, but all you have to ensure is its quality and
-                contents. Now, you might want to run a quick errand to buy olive oil, but it will not be just
-                olive oil that you will get - there will be several other oils and elements mixed. The reason
-                why we are focusing on oil and its contents, you ask? Our scalp absorbs everything. You
-                don’t want just anything to seep inside. Might do more harm than good. The quality of oil is
-                what directly impacts hair quality. Additionally, adulterated oil can mess with the pH level
-                of your scalp too. Nobody wants to attract attention to brittle tresses.
-                So, next time you buy a magic potion for your hair, be sure to flip it and read its ingredients.
-                It will only take a minute, come on.
+                ❖ Customizable as per your skin needs<br></br>
+                ❖ Naturally-driven ingredients with no side-effects <br></br>
+                ❖ Better for your skin in the long-term<br></br>
+                ❖ Better for the environment<br></br>
+                ❖ Nourishes and oxygenates skin<br></br>
+                ❖ Proper blood circulation<br></br>
+                ❖ Visibly radiant glow<br></br>
+                ❖ Ideal for sensitive skin <br />                
+                ❖ Stress buster <br />
+                ❖ Opens clogged pores
+                </span></div>
+            <div className="p-10 pb-0"><span className="font-normal text-2xl text-neutral-500 tracking-wide blogContent">Need more reasons for the shift? Ok, we promise glass clear skin too!</span></div>
+            <div className="p-10 pb-0"><span className="font-normal text-2xl text-cyan-600 tracking-wide blogContent">Don’t know where you can find an organic facial?</span></div>
+
+            <div className="p-10 pb-0"><span className="font-extralight leading-relaxed text-neutral-600 tracking-wide blogContent">A trip to Varnya Salons, Gurugram, promises an exceptional journey for your skin, hair and
+personal wellness. Along with the premium beauty services, one will also find a
+wonderfully-curated personal-care and wellness menu, both holistic and organic. Varnya is
+a budding name in the industry, but is growing and flourishing rapidly to be known for its
+holistic practices. Our boutique salon can be your one-stop solution when you seek luxury
+intertwined with Ayurvedic treatments. We have given a new meaning to self-preservation
+and self-care. Unwind on your day off with us and we promise to deliver an unmatched and
+unforgettable salon experience.
                 <br></br><br></br>
-                Love Instagram influencers’ hair games? Switch to virgin oils!</span></div>
+                Among all our services, the organic/chemical-free facial is a top favorite. Our certified
+estheticians possess the expertise to make you forget all your worries and bring you total
+relaxation with this facial.</span></div>
         </div>
     )
 }
